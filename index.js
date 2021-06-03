dba293f9613d7f73c0d28cdbe88d88f171d6a693
bugtype <bugtype.kr@gmail.com>
2021-06-03 22:06:20 +0900

    feat: 나이키 draw 셋팅

---
+++ index.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+const core = require('@actions/core');
+
+(async function () {
+  const url =
+    'https://www.nike.com/kr/launch/?type=upcoming&activeDate=date-filter:AFTER_DATE';
+  const { data } = await axios.default.get(url);
+  const isTodayNotification = data.includes('응모하기');
+  if (!isTodayNotification) {
+    core.setFailed('fail');
+  }
+})();
