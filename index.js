7fa94f46a1e3ecad0da5d3226303f4d05fc54640
Bugtype(Jerry) <bugtype.kr@gmail.com>
2021-06-05 14:24:06 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const axios = require('axios');
 const core = require('@actions/core');
 
 (async function () {
+  const currentDate = new Date();
+  console.log(currentDate, currentDate.toISOString());
+  
   const url =
     'https://www.nike.com/kr/launch/?type=upcoming&activeDate=date-filter:AFTER_DATE';
   const { data } = await axios.default.get(url);
